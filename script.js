d2aad29fe3fce5ecfd459ea657d792729b4857d7
nicolo_canevari <nico.canevari@outlook.it>
2024-12-18 14:48:57 +0100

    Fix let i = 1; faccio partire il contatore da 1 e non da 0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Vado a indicare al programma le istruzioni di iterazione per questo esercizio
-for (let i = 0; i <= 100; i++) {
+for (let i = 1; i <= 100; i++) {
 
     // controllo funzionamento del programma
     // console.log("Siamo all'iterazione numero: ", i);
