d00c4919b27c7e37443a5672040121df7a13711d
nicolo_canevari <nico.canevari@outlook.it>
2024-12-18 14:36:24 +0100

    Test FizzBuzz

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 // Vado a indicare al programma le istruzioni di iterazione per questo esercizio
 for (let i = 0; i <= 100; i++) {
 
-    // istruzioni da ripetere
+    // controllo funzionamento del programma
     console.log("Siamo all'iterazione numero: ", i);
+
+    // istruzioni da ripetere 
+    
+    // FizzBuzz , Fizz ,Buzz
+    if (i % 3 === 0 && i % 5 === 0) {
+
+        console.log("FizzBuzz");
+        
+    } 
     
 
 }
