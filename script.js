fe779947757056c06487655777a1edd3334337a0
nicolo_canevari <nico.canevari@outlook.it>
2024-12-18 14:45:49 +0100

    Fine esercizio

--- script.js
+++ script.js
@@ -4,21 +4,24 @@ for (let i = 0; i <= 100; i++) {
     // controllo funzionamento del programma
     // console.log("Siamo all'iterazione numero: ", i);
 
-    // istruzioni da ripetere 
+    // istruzioni da ripetere (cicle)
     
-    // FizzBuzz , Fizz , Buzz
     if (i % 3 === 0 && i % 5 === 0) {
-
+        // FizzBuzz
         console.log("FizzBuzz");
         
     } else if (i % 3 === 0) {
-
+        // Fizz
         console.log("Fizz");
 
     } else if (i % 5 === 0) {
-
+        // Buzz
         console.log("Buzz");
 
+    } else {
+        // iterazione (tutti i valori che non sono % di 3 e 5)
+        console.log(i);
+    
     }
 
 }
